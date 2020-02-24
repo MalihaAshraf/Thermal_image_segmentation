b9c7fa7672feb50004b2bd70912c8381d135c84b
Maliha Ashraf <malihaa266@gmail.com>
2020-02-24 20:45:11 +0000

    Auto read and preprocess

--- main.m
+++ main.m
@@ -2,126 +2,110 @@
 
 addpath('FLIR_class');
 
+% Browse to file
+[file,path] = uigetfile('*.*');
+
 % Read .ptw file
-data = FlirMovieReader('7p5mm_1000C_48hr_002/Rec-000523-001_00_26_22_883.ptw');
+data = FlirMovieReader([path file]);
+
 % File information
 vars = info(data);
 i = 1;
-rect = [194, 100, 74, 57];
-rect2 = [24 35 28 23];
+% rect = [194, 100, 74, 57];
+% rect2 = [24 35 28 23];
+
+% Choose rotation angle
+[frame, metadata] = read(data, 1);
+sprintf('Please click on any two points on the horizontal reference surface\nPress Enter to continue...')
+pause()
+figure, imshow(frame, [])
+[x, y] = ginput(2);
+close all
+angle = get_slope(x, y);
+clear x y
+
+% Choose region of interest
+frame_r = imRotateCrop(frame, angle);
+sprintf('Please drag rectangle across area of interest\nPress Enter to continue...')
+pause()
+figure, imshow(imadjust(im2double(frame_r)))
+rect = getrect();
+rect = round(rect);
+close all
+clear frame metadata frame_r
+
+data = FlirMovieReader([path file]);
+
+%% Read frames
 
 while ~isDone(data)
     [frame, metadata] = step(data);
-    frame_r = imrotate(frame, 2);
-    frame_c = frame_r(rect(2):rect(2)+rect(4), rect(1):rect(1)+rect(3));
-    frame_s = frame_c(rect2(2):rect2(2)+rect2(4), rect2(1):rect2(1)+rect2(3));
+    frame_r = imrotate(frame, angle);
+    d = 18;         % Adjust this number to fix cropping in x direction
+    frame_c = frame_r(rect(2):rect(2)+rect(4), rect(1)+d:rect(1)+rect(3));
+%     frame_s = frame_c(rect2(2):rect2(2)+rect2(4), rect2(1):rect2(1)+rect2(3));
     
     timelapse_raw(:,:,i) = frame;
     timelapse_cropped(:,:,i) = frame_c;
     MaxTemp(i, 1) = max(max(frame));
     MaxTemp(i, 2) = max(max(frame_c));
-    MaxTemp(i, 3) = max(max(frame_s));
+%     MaxTemp(i, 3) = max(max(frame_s));
     
     MeanTemp(i, 1) = mean(mean(frame));
     MeanTemp(i, 2) = mean(mean(frame_c));
-    MeanTemp(i, 3) = mean(mean(frame_s));
+%     MeanTemp(i, 3) = mean(mean(frame_s));
     
     i = i+1;
 end
 
 %% Initialize variables
 
-h_px = 29.89/7.41; % px/mm
-d_px = 38.875/7.55; % px/mm
+sprintf('Please click the top and bottom points of the sample\nPress Enter to continue...')
+pause()
+n = 1;          % Select the frame with correct initial height and width
+figure, imshow(timelapse_cropped(:,:,n), [])
+[x, y] = ginput(2);
+close all
+h = abs(y(2)-y(1));
+
+sprintf('Please click the right and left points of the sample\nPress Enter to continue...')
+pause()
+n = 1;          % Select the frame with correct initial height and width
+figure, imshow(timelapse_cropped(:,:,n), [])
+[x, y] = ginput(2);
+close all
+d = abs(x(2)-x(1));
+
+clear n x y
+
+h_px = h/7.41; % px/mm; Enter measured height
+d_px = d/7.55; % px/mm; Enter measured diameter
 
 %% Process images
 
-for i = 1:size(timelapse_cropped, 3)
+for i = 60:size(timelapse_cropped, 3)
    I = timelapse_cropped(:,:, i);
-   if i <=45
-       I_seg(:,:,i) = segment_image_low(I);
-   elseif i <=150 && i > 45
-       I_seg(:,:,i) = segment_image_mid(I);
-   else
-       I_seg(:,:,i) = segment_image_high(I);
-   end
+   I_seg(:,:,i) = segment_image_high(I);
    [vol(i), area(i)] = region_volume(I_seg(:,:,i), h_px, d_px);
    
 end
 
-%% Calculate porosity
+%% plot porosity
 
 vol_init = 0.331;
 t = 1:2861;
-p = (vol-vol_init).*100./vol;
+figure, scatter(t, (vol-vol_init).*100./vol, 3)
+% figure, scatter(t, vol, 3)
 
-%% Identify outliers
-
-% sel = (p < 80) | ((t <= 32) & (t > 80));
-% sel = (p < 75) | ((t <= 32) & (t > 50));
-sel = (p<75);
-options = fitoptions('Method','SmoothingSpline',...
-                     'SmoothingParam', 0.001);
-f1 = fit(t(sel)', p(sel)','poly9', 'Normalize','on','Robust','Bisquare');
-figure, plot(f1, t, p)
-
-% identify outliers based on fits
-fdata = feval(f1, t);
-err = abs(fdata - p') > 1*std(p');
-outliers = excludedata(t, p, 'indices', err);
-
-f2 = fit(t', p', 'poly9', 'Normalize','on','Robust','Bisquare',...
-    'Exclude', outliers);
-
-figure, plot(f2, t, p)
-
-% Model values
-p2 = feval(f2, t);
-v2 = 100*vol_init./(100-p2);
-
-%% Plots
-
-figure, scatter(t, p, 3)
 xlabel('Time in minutes')
 ylabel('Porosity %')
 
+%% plot surface area
+
+t = 1:2861;
 figure, scatter(t, area, 3)
+% figure, scatter(t, vol, 3)
+
 xlabel('Time in minutes')
 ylabel('Surface area cm^2')
-
-% Plots without outliers
-figure, scatter(t(~outliers), p(~outliers), 3)
-hold on,
-plot(t, p2);
-xlabel('Time in minutes')
-ylabel('Porosity %')
-
-figure, scatter(t(~outliers), area(~outliers), 3)
-xlabel('Time in minutes')
-ylabel('Surface area cm^2')
-
-%% Save data as CSV
-
-% Data with outliers
-ds1 = dataset();
-ds1.Time = t';
-ds1.Volume = vol';
-ds1.Porosity = p';
-ds1.Surface_Area = area';
-export(ds1, 'file', 'Furnace_data_with_outliers.csv', 'Delimiter', ',');
-
-% Data without outliers
-ds2 = dataset();
-ds2.Time = t(~outliers)';
-ds2.Volume = vol(~outliers)';
-ds2.Porosity = p(~outliers)';
-ds2.Surface_Area = area(~outliers)';
-export(ds2, 'file', 'Furnace_data_without_outliers.csv', 'Delimiter', ',');
-
-% Furnace data model fits
-ds3 = dataset();
-ds3.Time = t';
-ds3.Volume = v2;
-ds3.Porosity = p2;
-export(ds3, 'file', 'Furnace_data_model.csv', 'Delimiter', ',');
 
