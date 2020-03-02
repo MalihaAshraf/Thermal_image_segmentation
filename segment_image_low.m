28ced583a5125a8c58573eed7aebfc5e0e04a438
Maliha Ashraf <MalihaAshraf@users.noreply.github.com>
2020-03-02 12:16:06 +0000

    new segmentations@

--- segment_image_low.m
+++ segment_image_low.m
@@ -1,15 +1,50 @@
-function out = segment_image_low(img)
+function [out, y, x] = segment_image_low(img, mode, var)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-se = strel('disk',1);
+% nominal values:
+%       sesnitivity = 0.59
+%       erosion = 1;
+%       no. of pixels = 400
+% var = [sensitivity, erosion, no. of pixels
+
+se = strel('disk', var(2));
 se2 = strel('disk',2);
 
-I = imerode(imdilate(imfill(imfill(bwareaopen(imerode(imcomplement(imbinarize...
-    (imadjust(medfilt2(im2double(img))), 'adapt', 'Sensitivity', 0.59)),... 
-    se), 400), [40 40] ), [35, 45]), se2), se);
+if (strcmp(mode, 'debug'))
+    figure, imshow(img, [])
+    I1 = imbinarize(imadjust(medfilt2(im2double(img))), 'adapt', 'Sensitivity', var(1));
+    figure, imshow(I1), title('1. binarize')
+    I2 = imcomplement(I1);
+    figure, imshow(I2), title('2. complement')
+    I3 = imerode(I2, se);
+    figure, imshow(I3), title('3. erode')
+    I4 = bwareaopen(I3, var(3));
+    figure, imshow(I4), title('4. area open')
+    
+    sprintf('Please click the centre of the sample\nPress Enter to continue...')
+    pause()
+    [x, y] = ginput(1);
+    
+    I5 = imfill(I4, round([y x]));
+    figure, imshow(I5), title('5. fill')
+%     I6 = imfill(I5, [35, 45]);
+%     figure, imshow(I6), title('6. fill')
+    I6 = imdilate(I5, se);
+%     I8 = imerode(I7, se2);
+    I = I6;
+    figure, imshow(I), title('final');
+    out = I;
+else
+    y = round(var(4)); x = round(var(5));
+    I = imdilate(imfill(imfill(bwareaopen(imerode(imcomplement(imbinarize...
+    (imadjust(medfilt2(im2double(img))), 'adapt', 'Sensitivity', var(1))),... 
+    se), var(3)), [y, x] ), [y, x]), se);
+    
+    
+    out = I;
+end
 
-out = I;
 
 end
 
--- segment_image_mid.m
+++ segment_image_mid.m
@@ -1,12 +1,42 @@
-function out = segment_image_high(img)
+function out = segment_image_mid(img, mode, var)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-se = strel('disk',1);
-I = imerode(imerode(imdilate(imfill(imdilate(bwareaopen(imbinarize(imadjust(medfilt2(im2double(img))),... 
-    'adapt', 'Sensitivity', 0.59), 400), se), [40 40] ), se), se), se);
 
-out = I;
+% nominal values:
+%       sesnitivity = 0.59
+%       erosion = 1;
+%       no. of pixels = 400
+% var = [sensitivity, erosion, no. of pixels
+
+y = round(var(4)); x = round(var(5));
+se = strel('disk', var(2));
+
+if (strcmp(mode, 'debug'))
+    figure, imshow(img, [])
+    I1 = imbinarize(imadjust(medfilt2(im2double(img))), 'adapt', 'Sensitivity', var(1));
+    figure, imshow(I1), title('1. binarize')
+    I2 = imerode(I1, se);
+    figure, imshow(I2), title('2. erode')
+    I3 = bwareaopen(I2, var(3));
+    figure, imshow(I3), title('3. area open')
+    I4 = imdilate(I3, se);
+    figure, imshow(I4), title('4. dilate')
+    I5 = imfill(I4, [y x]);
+    figure, imshow(I5), title('5. fill');
+    I6 = imdilate(I5, se);
+    I7 = imerode(I6, se);
+%     I8 = imerode(I7,se);
+    out = I7;
+    figure, imshow(out), title('final')
+else
+    
+    I = imerode(imdilate(imfill(imdilate(bwareaopen(imerode(imbinarize(imadjust(medfilt2(im2double(img))),... 
+    'adapt', 'Sensitivity', var(1)), se), var(3)), se), [y x] ), se), se);
+
+    out = I;
+end
+
 
 end
 
